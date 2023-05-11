261bafc1ca3af6a9041f6be0bed406de6f29acf6
Adolfo Ochagavía <github@adolfo.ochagavia.nl>
2023-05-11 14:21:30 +0200

    Fix Rust code example

--- 2023-you-are-holding-it-wrong/parallel-count-rs/src/main.rs
+++ 2023-you-are-holding-it-wrong/parallel-count-rs/src/main.rs
@@ -1,25 +1,26 @@
 use std::thread;
+use std::sync::atomic::{AtomicU32, Ordering};
 
 fn main() {
-    let mut counter = 0;
+    let counter = AtomicU32::new(0);
 
     thread::scope(|s| {
         let t1 = s.spawn(|| {
             for _ in 0..50000 {
-                counter += 1;
+                counter.fetch_add(1, Ordering::Relaxed);
             }
         });
 
         let t2 = s.spawn(|| {
             for _ in 0..50000 {
-                counter += 1;
+                counter.fetch_add(1, Ordering::Relaxed);
             }
         });
 
         t1.join().unwrap();
         t2.join().unwrap();
 
-        println!("Result = {counter}");
+        println!("Result = {}", counter.load(Ordering::Relaxed));
     });
 
 }
